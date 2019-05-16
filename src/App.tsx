e6f9e17bbdec76cc2054218055d9d334fe47b5ff
Larry Faudree <larryfaudree@larrys-mbp.attlocal.net>
2019-05-15 22:17:07 -0400

    navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Sidebar from './components/sidebar/Sidebar';
 import './App.sass';
+import USLogo from './media/United-States.png'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 library.add(fab);
 library.add(fas);
@@ -12,18 +15,72 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="columns max-height m-0 p-0">
+				<div className="columns is-gapless max-height m-0 p-0">
 					<div className="column is-2 m-0 p-0">
 						<Sidebar />
 					</div>
 					<div className="column m-0 p-0">
-						<nav className="navbar app-navbar is-dark" role="navigation">
+						<nav className="navbar app-navbar is-transparent is-dark" role="navigation">
 							<div className="navbar-menu">
 								<div className="navbar-start" />
 								<div className="navbar-end">
-									<a className="navbar-item">Home</a>
-									<a className="navbar-item">Documentation</a>
-									<a className="navbar-item">More</a>
+									<div className="navbar-section-divider"></div>
+									<div className="navbar-item navbar-section">
+										<div className="navbar-link is-arrowless">
+											<div className="theme-icon theme-blue"></div>
+										</div>
+									</div>
+									<div className="navbar-section-divider"></div>
+									<div className="navbar-item navbar-section has-dropdown is-hoverable">
+										<a className="navbar-link is-arrowless">
+											<img src={USLogo} alt="United States Flag"/>
+										</a>
+										<div className="navbar-dropdown is-boxed">
+											<a className="navbar-item">
+												<img src={USLogo} alt="United States Flag"/>
+												<span className="ml-1"> English - U.S.</span>
+											</a>
+										</div>
+									</div>
+									<div className="navbar-section-divider"></div>
+									<div className="navbar-section">
+										<div className="navbar-item">
+											<span className="has-text-grey-light">
+												<FontAwesomeIcon icon="user"></FontAwesomeIcon>
+											</span>
+										</div>
+										<div className="navbar-item">
+											<span className="has-text-grey-light">
+												<FontAwesomeIcon icon="cog"></FontAwesomeIcon>
+											</span>
+										</div>
+									</div>
+									<div className="navbar-section-divider"></div>
+									<div className="navbar-section">
+										<div className="navbar-item">
+											<span className="has-text-grey-light">
+												<FontAwesomeIcon icon="envelope"></FontAwesomeIcon>
+											</span>
+										</div>
+										<div className="navbar-item">
+											<span className="has-text-grey-light">
+												<FontAwesomeIcon icon="bullhorn"></FontAwesomeIcon>
+											</span>
+										</div>
+										<div className="navbar-item">
+											<span className="has-text-grey-light">
+												<FontAwesomeIcon icon="rss"></FontAwesomeIcon>
+											</span>
+										</div>
+									</div>
+									<div className="navbar-section-divider"></div>
+									<a className="navbar-section has-background-danger">
+										<div className="navbar-item">
+											<span className="has-text-light">
+												<FontAwesomeIcon icon="power-off"></FontAwesomeIcon>
+											</span>
+										</div>
+									</a>
 								</div>
 							</div>
 						</nav>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SidebarTabs from './tabs/SidebarTabs';
+import larry from '../../media/larry.jpg'
 import './Sidebar.sass';
 
 function Sidebar(props: object) {
+	const [isLocked, updateLock] = useState(false)
+
 	return (
 		<div className="app-sidebar">
 			<div className="app-sidebar__header">
 				<figure className="user-avatar image is-48x48">
-					<img className="is-rounded" src="https://larryfaudree.surge.sh/static/img/ME%20(3).e9132be.jpg" />
+					<img className="is-rounded" src={larry} />
 				</figure>
 				<div className="member-status">
 					<h3>Larry Faudree Jr</h3>
@@ -16,8 +19,10 @@ function Sidebar(props: object) {
 						<div className="progress-bar__progress" />
 					</div>
 				</div>
-				<div className="lock">
-					<FontAwesomeIcon icon={[ 'fas', 'unlock' ]} />
+				<div className="lock" onClick={() => updateLock(!isLocked)}>
+					{
+						isLocked ? <FontAwesomeIcon icon={[ 'fas', 'lock' ]} /> : <FontAwesomeIcon icon={[ 'fas', 'unlock' ]} />
+					}
 				</div>
 			</div>
 			<div className="app-sidebar__content">
